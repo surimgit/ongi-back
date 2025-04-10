36c564b7545c0bba5ac58c3dbd71701b613e6d02
YooKyuBong <58059368+ykb-i@users.noreply.github.com>
2025-04-10 16:45:19 +0900

    branch 생성

--- src/main/java/com/ongi/ongi_back/OngiBackApplication.java
+++ src/main/java/com/ongi/ongi_back/OngiBackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OngiBackApplication {
 
-	// branch 생성 sdaf
+	// branch 생성
 	public static void main(String[] args) {
 		SpringApplication.run(OngiBackApplication.class, args);
 	}
